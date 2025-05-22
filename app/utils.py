340bf36e3a185d4609c65bffa676a84cbf39027a
Cassie Buhler <cassidy.buhler@gmail.com>
2025-05-22 23:11:36 +0000

    revamping the app!

---
+++ app/utils.py
@@ -0,0 +1,177 @@
+import ibis
+from ibis import _
+from variables import *
+import altair as alt
+import re
+
+def get_counties(state_selection):
+    if state_selection != 'All':
+        counties = database.filter(_.state_name == state_selection).select('county').distinct().order_by('county').execute()
+        counties = ['All'] + counties['county'].tolist()
+    else: 
+        counties = None
+    return counties
+     
+def filter_data(table, state_choice, county_choice, year_range):
+    county_choice = re.sub(r"(?i)\s*(County)\b", "", county_choice)    
+    min_year, max_year = year_range
+    gdf = (table.filter(_.year>=(min_year))
+           .filter(_.year<=(max_year))
+          )
+    if state_choice[0] != "All":
+        gdf = gdf.filter(_.state_name.isin(state_choice))
+        if (county_choice != "All") and (county_choice[0]):
+            gdf = gdf.filter(_.county == county_choice)
+    return gdf
+
+def group_data(table, style_choice):
+    metric_col = style_choice_columns[style_choice]
+    gdf = table.group_by(_.year).agg(total_amount = _[metric_col].sum())
+    return gdf
+    
+
+def fit_bounds(state_choice, county_choice, m):
+    if state_choice[0] != "All":
+        gdf = county_bounds.filter(_.state_name.isin(state_choice))
+        if (county_choice != "All") and (county_choice[0]):
+            gdf = gdf.filter(_.county == county_choice)
+        bounds = list(gdf.execute().total_bounds)
+        m.fit_bounds(bounds) # need to zoom to filtered area
+        return
+
+
+def get_bar(df, style_choice, group_col, metric_col, paint, x_lab, y_lab, title):
+    if isinstance(paint, dict):
+        domain = [stop[0] for stop in paint['stops']]
+        range_ = [stop[1] for stop in paint['stops']]
+    
+    chart = (alt.Chart(df)
+                .mark_bar(stroke="black", strokeWidth=0.1)
+                .encode(
+                    x=alt.X(f"{group_col}:N", axis=alt.Axis(title=x_lab)),
+                    y=alt.Y(f"{metric_col}:Q", axis=alt.Axis(title=y_lab)),
+                    tooltip=[alt.Tooltip(group_col, type="nominal"), alt.Tooltip(metric_col, type="quantitative")],
+                    # color=alt.Color(
+                    #         f"{group_col}:N",
+                    #         legend=alt.Legend(title=style_choice,symbolStrokeWidth=0.5),
+                    #         scale=alt.Scale(domain=domain, range=range_)
+                    #      ),
+                    )
+             .properties(title=f"{title}")
+            )
+    st.altair_chart(chart, use_container_width = True)
+    return 
+    
+def tpl_style(ids, paint):
+    style =  {
+    "version": 8,
+    "sources": {
+        "tpl": {
+            "type": "vector",
+            "url": "pmtiles://" + pmtiles,
+            "attribution": "TPL"
+        },
+    },
+    "layers": [{
+            "id": "tpl",
+            "source": "tpl",
+            "source-layer": source_layer_name,
+            "type": "fill",
+            'filter': ['in', ['get', 'TPL_ID'], ["literal", ids]],
+            "paint": {
+                "fill-color": paint,
+                "fill-opacity": 1
+            }
+        }]
+    }
+    return style
+
+    
+def get_legend(paint):
+    """
+    Generates a legend dictionary with color mapping and formatting adjustments.
+    """
+    legend = {cat: color for cat, color in paint['stops']}
+    position, fontsize, bg_color = 'bottom-left', 15, 'white'
+    bg_color = 'rgba(255, 255, 255, 0.6)'
+    fontsize = 12
+    return legend, position, bg_color, fontsize
+
+
+
+@st.cache_data
+def tpl_summary(_df):
+    summary = _df.group_by(_.Manager_Type).agg(Amount = _.Amount.sum())
+    # summary = _df.filter(~_.Amount.isnull()).group_by(_.Manager_Type).agg(Amount = _.Amount.sum())
+    public_dollars = round( summary.filter(_.Manager_Type.isin(["FED", "STAT", "LOC", "DIST"])).agg(total = _.Amount.sum()).to_pandas().values[0][0] )
+    private_dollars = round( summary.filter(_.Manager_Type.isin(["PVT", "NGO"])).agg(total = _.Amount.sum()).to_pandas().values[0][0] )
+    # tribal_dollars = summary.filter(_.Manager_Type.isin(["TRIB"])).agg(total = _.Amount.sum()).to_pandas().values[0][0] 
+    # tribal_dollars = tribal_dollars if tribal_dollars else round(tribal_dollars)
+    total_dollars = round( summary.agg(total = _.Amount.sum()).to_pandas().values[0][0] )
+    return public_dollars, private_dollars, total_dollars
+
+# @st.cache_data
+def calc_delta(_df):
+    deltas = (_df
+     .group_by(_.Manager_Type, _.year)
+     .agg(Amount = _.Amount.sum())
+     .mutate(total = _.Amount.cumsum(order_by=_.year, group_by=_.Manager_Type))
+     .mutate(lag = _.total.lag(1))
+     .mutate(delta = (100*(_.total - _.lag) / _.total).round(2)  )
+     .filter(_.year >=2019)
+     .select(_.Manager_Type, _.year, _.total, _.lag, _.delta)
+    )
+    public_delta = deltas.filter(_.Manager_Type.isin(["FED", "STAT", "LOC", "DIST"])).to_pandas()
+    public_delta =  0 if public_delta.empty else public_delta.delta[0]
+
+    private_delta = deltas.filter(_.Manager_Type.isin(["PVT", "NGO"])).to_pandas()
+    private_delta =  0 if private_delta.empty else private_delta.delta[0]
+    
+    return public_delta, private_delta
+    
+# @st.cache_data
+def get_area_totals(_df, column):
+    return _df.group_by(_[column]).agg(area = _.Shape_Area.sum() / (100*100)).to_pandas()
+
+
+# @st.cache_data
+def bar(area_totals, column, paint):
+    # domain = [stop[0] for stop in paint['stops']]
+    # range_ = [stop[1] for stop in paint['stops']]
+    plt = alt.Chart(area_totals).mark_bar().encode(
+            x=column,
+            y=alt.Y("area"),
+            # color=alt.Color(column).scale(domain = domain, range = range_)
+        ).properties(height=350)
+    return plt
+#bar
+
+# +
+
+# @st.cache_data
+# def calc_timeseries(_df, column):
+#     timeseries = (
+#         _df
+#         .filter(~_.year.isnull())
+#         .filter(_.year > 0)
+#         .group_by([_.year, _[column]])
+#         .agg(Amount = _.Amount.sum())
+#         .mutate(year = _.year.cast("int"),
+#                 Amount = _.Amount.cumsum(group_by=_[column], order_by=_.year))
+        
+#         .to_pandas()
+#     )
+#     return timeseries
+
+
+# @st.cache_data
+def chart_time(timeseries, column, paint):
+    domain = [stop[0] for stop in paint['stops']]
+    range_ = [stop[1] for stop in paint['stops']]
+    # use the colors 
+    plt = alt.Chart(timeseries).mark_line().encode(
+        x='year:O',
+        y = alt.Y('Amount:Q'),
+            color=alt.Color(column,scale= alt.Scale(domain=domain, range=range_))
+    ).properties(height=350)
+    return plt
---
+++ app/variables.py
@@ -0,0 +1,202 @@
+import os
+import ibis
+from ibis import _
+import ibis.selectors as s
+from cng.utils import *
+from cng.h3 import *
+from minio import Minio
+import streamlit 
+from datetime import timedelta
+import re
+duckdb_install_h3()
+
+# con = ibis.duckdb.connect("duck.db",extensions = ["spatial", "h3"])
+con = ibis.duckdb.connect(extensions = ["spatial", "h3"])
+set_secrets(con)
+
+# Get signed URLs to access license-controlled layers
+key = st.secrets["MINIO_KEY"]
+secret = st.secrets["MINIO_SECRET"]
+client = Minio("minio.carlboettiger.info", key, secret)
+
+tracts_z8 = con.read_parquet("https://minio.carlboettiger.info/public-social-vulnerability/2022-tracts-h3-z8.parquet").select('FIPS','h8').mutate(h8 = _.h8.lower()).rename(FIPS_tract = "FIPS")
+pad_z8 = con.read_parquet("https://minio.carlboettiger.info/public-biodiversity/pad-us-4/pad-h3-z8.parquet")
+mobi = con.read_parquet("https://minio.carlboettiger.info/public-mobi/hex/all-richness-h8.parquet").select("h8", "Z").rename(richness = "Z")
+svi = con.read_parquet("https://minio.carlboettiger.info/public-social-vulnerability/2022/SVI2022_US_tract.parquet").select("FIPS", "RPL_THEMES").filter(_.RPL_THEMES > 0).rename(svi = "RPL_THEMES").rename(FIPS_tract = "FIPS")
+
+tpl_geom_url = "s3://shared-tpl/tpl.parquet"
+tpl_table = con.read_parquet(tpl_geom_url).mutate(geom = _.geom.convert("ESRI:102039", "EPSG:4326")).rename(year = 'Close_Year', state_name = 'State', county = 'County')
+
+county_bounds = con.read_parquet("https://minio.carlboettiger.info/public-census/2024/county/2024_us_county.parquet")
+landvote_z8 = (con.read_parquet("s3://shared-tpl/landvote_h3_z8.parquet")
+            .rename(FIPS_county = "FIPS", measure_amount = 'Conservation Funds Approved', 
+                    measure_status = "Status", measure_purpose = "Purpose",)
+            .mutate(measure_year = _.Date.year()).drop('Date','geom'))
+
+
+landvote_table = (con.read_parquet("s3://shared-tpl/landvote_geom.parquet")
+            .rename(FIPS_county = "FIPS", measure_amount = 'Conservation Funds Approved', 
+                    measure_status = "Status", measure_purpose = "Purpose")
+            .mutate(year = _.Date.year()).drop('Date'))
+
+
+tpl_drop_cols = ['Reported_Acres','Close_Date','EasementHolder_Name',
+        'Data_Provider','Data_Source','Data_Aggregator',
+        'Program_ID','Sponsor_ID']
+tpl_z8_url = "s3://shared-tpl/tpl_h3_z8.parquet"
+tpl_z8 = con.read_parquet(tpl_z8_url).mutate(h8 = _.h8.lower()).drop(tpl_drop_cols)
+
+select_cols = ['fid','TPL_ID','landvote_id',
+'state','state_name','county',
+ 'FIPS_county', 'FIPS_tract',
+ 'city','jurisdiction',
+ 'Close_Year', 'Site_Name',
+ 'Owner_Name','Owner_Type',
+ 'Manager_Name','Manager_Type',
+ 'Purchase_Type','EasementHolder_Type',
+ 'Public_Access_Type','Purpose_Type',
+ 'Duration_Type','Amount',
+ 'Program_Name','Sponsor_Name',
+ 'Sponsor_Type','measure_year',
+ 'measure_status','measure_purpose',
+ 'measure_amount',
+ 'richness','svi',
+ 'h8']
+
+ # 'h8','geom']
+  # ]
+
+
+database = (
+  tpl_z8.drop('State','County')
+  .left_join(landvote_z8, "h8").drop('h8_right')
+  .left_join(mobi, "h8").drop('h8_right')
+  # .left_join(pad_z8, "h8").drop('h8_right')
+  # .inner_join(pad_us, "row_n")
+  .left_join(tracts_z8, "h8").drop('h8_right')
+  .inner_join(svi, "FIPS_tract")
+).select(select_cols).distinct()
+
+database_geom = (database.drop('h8').distinct().inner_join(tpl_table.select('geom','TPL_ID','fid','Shape_Area'), [database.TPL_ID == tpl_table.TPL_ID, database.fid == tpl_table.fid])
+            .mutate(acres = _.Shape_Area*0.0002471054)
+            # .mutate(area = _.geom.area())
+           ).distinct()
+
+pmtiles = client.get_presigned_url(
+    "GET",
+    "shared-tpl",
+    "tpl_v2.pmtiles",
+    expires=timedelta(hours=2),
+)
+# source_layer_name = re.sub(r'\W+', '', os.path.splitext(os.path.basename(pmtiles))[0]) #stripping hyphens to get layer name 
+source_layer_name = 'tpl'
+
+
+states = (
+    "All", "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut",
+    "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa",
+    "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan",
+    "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire",
+    "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio",
+    "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina",
+    "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington",
+    "West Virginia", "Wisconsin", "Wyoming"
+)
+
+# Define color hex codes
+darkblue = "#00008B"
+blue = "#0096FF"
+lightblue = "#ADD8E6"
+darkgreen = "#006400"
+grey = "#c4c3c3"
+dark_grey = "#5a5a5a"
+green = "#008000"
+purple = "#800080"
+darkred = "#8B0000"
+orange = "#FFA500"
+red = "#e64242"
+yellow = "#FFFF00"
+pink = '#FFC0CB'
+
+style_options = {
+    "Acquisition Cost":  
+            ["interpolate",
+                ['exponential', 1], 
+                ["get", "Amount"],
+                    0,	"#fde725",
+                    36000,	"#b4de2c",
+                    93000,	"#6ccd59",
+                    156500,	"#35b779",
+                    240000,	"#1f9e89",
+                    335000,	"#26828e",
+                    500000,	"#31688e",
+                    796314,	"#3e4a89",
+                    1634000,	"#482878",
+                    308461380,	"#430154",
+                    ] 
+            ,
+    "Manager Type":  {
+            'property': 'Manager_Type',
+            'type': 'categorical',
+            'stops': [
+                ['FED', darkblue],
+                ['STAT', blue],
+                ['LOC', lightblue],
+                ['DIST', darkgreen],
+                ['UNK', dark_grey],
+                ['JNT', pink],
+                ['TRIB', purple],
+                ['PVT', darkred],
+                ['NGO', orange]
+            ]
+            },
+    "Access": {
+        'property': 'Public_Access_Type',
+        'type': 'categorical',
+        'stops': [
+            ['OA', green],
+            ['XA', red],
+            ['UK', dark_grey],
+            ['RA', orange]
+        ]
+    },
+    "Purpose": {
+        'property': 'Purpose_Type',
+        'type': 'categorical',
+        'stops': [
+            ['FOR', green],
+            ['HIST', red],
+            ['UNK', dark_grey],
+            ['OTH', grey],
+            ['FARM', yellow],
+            ['REC', blue],
+            ['ENV', purple],
+            ['SCE', orange],
+            ['RAN', pink]
+        ]
+    },
+}
+
+style_choice_columns = {'Manager Type': style_options['Manager Type']['property'],
+              'Access' : style_options['Access']['property'],
+              'Purpose': style_options['Purpose']['property'],
+                'Acquisition Cost': 'Amount',
+                'Measure Cost': 'measure_amount',
+             }
+
+# metric_columns = {'svi': 'svi', 'mobi': 'richness', 'landvote':'measure_status'}
+
+from langchain_openai import ChatOpenAI
+import streamlit as st
+
+## dockerized streamlit app wants to read from os.getenv(), otherwise use st.secrets
+import os
+api_key = os.getenv("NRP_API_KEY")
+if api_key is None:
+    api_key = st.secrets["NRP_API_KEY"]
+
+llm_options = {
+    "llama3.3": ChatOpenAI(model = "llama3-sdsc", api_key=api_key, base_url = "https://llm.nrp-nautilus.io/",  temperature=0),
+    "gemma3": ChatOpenAI(model = "gemma3", api_key=api_key, base_url = "https://llm.nrp-nautilus.io/",  temperature=0),
+    "watt": ChatOpenAI(model = "watt", api_key=api_key, base_url = "https://llm.nrp-nautilus.io/",  temperature=0),
+}
